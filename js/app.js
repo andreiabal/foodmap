203b7fc8e90773f164eb9e35e456a79dd5d85d7d
Andreia Bal <bal.andreia@gmail.com>
2018-08-04 23:32:15 -0300

    com filtro

--- js/app.js
+++ js/app.js
@@ -1,57 +1,77 @@
-$(document).ready(function() {
-  $('.box-2').delay('3000').fadeIn('slow')
-
-  //Imagens
-$.each(restaurantes, function (index, restaurante) {
-  //$("<img>").attr("src", restaurante.image).addClass(restaurante.name).appendTo("#box-images");
-  $("<img>").attr("src", restaurante.image).attr('id', restaurante.name).attr('onClick', 'onClick(this);').addClass(restaurante.type).appendTo("#box-images");
-  $(".form-control").focusout(function(){
-    var busca = $(this).val();
-    //console.log(busca);
+// O mapa é iniciado pelo callback do carregamento do Google Maps
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: new google.maps.LatLng(-23.5576364, -46.6644888),
+    mapTypeId: 'roadmap'
   });
-})
-});
 
-
-/*var map;
-
-function initMap() {
- map = new google.maps.Map(document.getElementById('map'), {
-   center: {lat: -23.5676623, lng: -46.6601063},
-   zoom: 15
- });
-}*/
-
-
-//MAPA//
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 14,
-  center: new google.maps.LatLng(-23.5576364, -46.6644888),
-  mapTypeId: 'roadmap'
-});
-
-  /*var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-  var icons = {
-    info: {
-      icon: iconBase + 'info-i_maps.png'
-    }
-  };*/
-
-  var features = restaurantes.map(function(r){
+  const features = restaurantes.map(function(r){
     return {
       position: new google.maps.LatLng(r.latitude, r.longitude),
       type: 'info'
     }
   });
 
-  // Create markers.
+  // Marcadores
   features.forEach(function(feature) {
-    var marker = new google.maps.Marker({
+    new google.maps.Marker({
       position: feature.position,
-      //icon: icons[feature.type],
       map: map
     });
   });
 }
+
+function hideSplashScreen() {
+  $('#main-content').delay('2000').fadeIn('slow')
+}
+
+function showRestaurants(restaurantsArray) {
+  $.each(restaurantsArray, function (index, restaurant) {
+    $('<img>')
+      .attr('src', restaurant.image)
+      .attr('id', restaurant.name)
+      .attr('onClick', 'openModal')
+      .addClass(restaurant.type)
+      .appendTo('#box-images');
+  })
+}
+
+function initialiseSearchInput() {
+  $('.form-control').keyup(function() {
+    const search = $(this).val().toLowerCase();
+
+    // Mesma coisa que:
+    //
+    // const filteredRestaurants = []
+    // for (let i = 0; i < restaurantes.length) {
+    //   if (restaurantes[i].name === search) {
+    //     filteredRestaurants.push(restaurantes[i]);
+    //   }
+    // }
+    const filteredRestaurants = 
+      $(restaurantes).filter(function (index, restaurant) { 
+        // Quando a busca for vazia, mostro todos os restaurantes
+        if (search === '') { 
+          return true;
+        }
+
+        if (restaurant.type.toLowerCase() === search) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+
+    $('#box-images').html('');
+
+    showRestaurants(filteredRestaurants);
+  });
+}
+
+// Inicialização da aplicação
+$(document).ready(function() {
+  hideSplashScreen();
+  showRestaurants(restaurantes);
+  initialiseSearchInput();
+});
